5a9c7465f3de4030b5a761ec4c82c1def9cca1ac
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2016-12-22 13:20:44 +1100

    Code splitting with React Router

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import Router from './Router';
+import {Router, browserHistory} from 'react-router';
+import ReactGA from 'react-ga';
+import routes from './pages/routes';
 import store from '../core/store';
 
 export default class Main extends Component {
+  componentWillMount(){
+    ReactGA.initialize('UA-79882435-1');
+  }
+  logPageView() {
+    window.scrollTo(0, 0)
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  }
   render() {
     return (
       <Provider store={store}>
-        <Router />
+        <Router history={browserHistory} onUpdate={this.logPageView} routes={routes} />
       </Provider>
     )
   }
