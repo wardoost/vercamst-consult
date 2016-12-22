fdcbcc648453749ea019a4e54886cc51f06b8b0d
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2016-12-22 15:00:56 +1100

    Firebase configuration in .env file

--- config/env.js
+++ config/env.js
@@ -20,7 +20,12 @@ function getClientEnvironment(publicUrl) {
       // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
       // This should only be used as an escape hatch. Normally you would put
       // images into the `src` and `import` them in code to get their paths.
-      'process.env.PUBLIC_URL': JSON.stringify(publicUrl)
+      'process.env.PUBLIC_URL': JSON.stringify(publicUrl),
+      // Firebase configuration
+      'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.NODE_ENV === 'development' ? process.env.FIREBASE_API_KEY_DEV : process.env.FIREBASE_API_KEY_PROD),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.NODE_ENV === 'development' ? process.env.FIREBASE_AUTH_DOMAIN_DEV : process.env.FIREBASE_AUTH_DOMAIN_PROD),
+      'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.NODE_ENV === 'development' ? process.env.FIREBASE_DATABASE_URL_DEV : process.env.FIREBASE_DATABASE_URL_PROD),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.NODE_ENV === 'development' ? process.env.FIREBASE_STORAGE_BUCKET_DEV : process.env.FIREBASE_STORAGE_BUCKET_PROD)
     });
 }
 
--- src/core/firebase/firebase.js
+++ src/core/firebase/firebase.js
@@ -1,10 +1,10 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAliWYXAbSVu9HHpHLHDDTmC-I1UOAq_1k',
-  authDomain: 'vercamst-consult.firebaseapp.com',
-  databaseURL: 'https://vercamst-consult.firebaseio.com',
-  storageBucket: 'vercamst-consult.appspot.com',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 }
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
