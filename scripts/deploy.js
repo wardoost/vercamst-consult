c138831d3fcaaf8591391c8a5359bdf82104f630
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2017-01-14 03:09:27 +1100

    Ftp upload for deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,20 +9,53 @@ require('dotenv').config({silent: true});
 
 var fs = require('fs');
 var childProcess = require('child_process');
+var path = require('path');
 var chalk = require('chalk');
+var ftp = require('scp2')
+var Client = require('scp2').Client
+var paths = require('../config/paths');
 
 // Run build script and deploy database rules
 childProcess.fork('scripts/build.js').on('exit', function (err) {
   if (err) throw err;
-  console.log();
-  console.log('Deploying Firebase rules to ' + chalk.cyan(process.env.FIREBASE_PROJECT_ID) + '...')
-  childProcess.exec(`firebase deploy --only database --project ${process.env.FIREBASE_PROJECT_ID}`, (error, stdout, stderr) => {
-    if (error) {
-      console.log();
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(chalk.green('Database rules deployed successfully.'));
+    console.log('The ' + chalk.cyan('build') + ' folder is ready to be deployed. Starting file transfer...');
     console.log();
+
+    var ftpClient = new Client({
+      host: process.env.FTP_HOST,
+      port: process.env.FTP_PORT,
+      username: process.env.FTP_USER,
+      password: process.env.FTP_PASSWORD,
+    });
+
+    ftpClient.on('write', function(options) {
+      console.log(
+        '  ' +
+        chalk.dim('build' + path.dirname(options.source).replace(paths.appBuild, '') + path.sep) +
+        chalk.cyan(path.basename(options.source)) +
+        ' -> ' +
+        chalk.dim(path.dirname(options.destination) + path.sep) +
+        chalk.cyan(path.basename(options.destination))
+      );
+    })
+
+    ftp.scp(paths.appBuild, {path: process.env.FTP_PATH}, ftpClient, function(err) {
+      if (err) throw err;
+      console.log();
+      console.log(chalk.green('All files transfered successfully.'));
+      console.log();
+
+      console.log('Deploying Firebase rules to ' + chalk.cyan(process.env.FIREBASE_PROJECT_ID) + '...')
+      childProcess.exec(`firebase deploy --only database --project ${process.env.FIREBASE_PROJECT_ID}`, (error, stdout, stderr) => {
+        if (error) {
+          console.log();
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(chalk.green('Database rules deployed successfully.'));
+        console.log();
+        console.log('👏  ' + chalk.green('Deployment successfull') + ' 👏');
+        console.log();
+    })
   });
 });
